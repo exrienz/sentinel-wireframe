389ac107180450fae9f37c719c7e9ca5080a87bb
Exrienz <exrienz@gmail.com>
2025-02-14 11:25:59 +0800

    first commit
    first commit

---
+++ src/assets/static/js/pages/ckeditor.js
@@ -0,0 +1,7 @@
+ClassicEditor.create(document.querySelector("#editor")).catch((error) => {
+  console.error(error)
+})
+
+ClassicEditor.create(document.querySelector("#editor2")).catch((error) => {
+  console.error(error)
+})
